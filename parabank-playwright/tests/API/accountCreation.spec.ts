c488ff86fcf3f1caa5033a44b3861bf37a89f3f9
Devansh-Bhatra <devanshbhatra2614@gmail.com>
2026-06-13 19:23:11 +0530

    Fix: dynamic customerId in API tests, fix Allure command

--- parabank-playwright/tests/API/accountCreation.spec.ts
+++ parabank-playwright/tests/API/accountCreation.spec.ts
@@ -1,15 +1,30 @@
 import { test, expect } from '@playwright/test'
+import * as fs from 'fs'
+import * as path from 'path'
 
 test('Create New Account API', async ({ request }) => {
 
+    // Read dynamic values written by OpeningAccount.spec.ts (UI test)
+    const checkingData = JSON.parse(
+        fs.readFileSync(
+            path.join(__dirname, '../../Utils/checkingAcc.json'),
+            'utf-8'
+        )
+    )
+
+    const customerId   = checkingData.customerId
+    const fromAccountId = checkingData.accountId
+
+    console.log('customerId =', customerId)
+    console.log('fromAccountId =', fromAccountId)
+
     const response = await request.post(
-        'https://parabank.parasoft.com/parabank/services/bank/createAccount?customerId=14210&newAccountType=1&fromAccountId=16119'
+        `https://parabank.parasoft.com/parabank/services/bank/createAccount?customerId=${customerId}&newAccountType=1&fromAccountId=${fromAccountId}`
     )
 
     console.log('Status =', response.status())
 
     const body = await response.text()
-
     console.log('Response =')
     console.log(body)
 
